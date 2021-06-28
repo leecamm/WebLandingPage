8bcf3212d8594c1fe9e0100b1f5917eda41153d7
leecamm <leduchieu642@gmail.com>
2021-06-28 10:49:00 +0200

    Upload Web Landing Page

---
+++ resources/js/script.js
@@ -0,0 +1,40 @@
+$(document).ready(
+    function(){
+
+        // Sticky nav
+        $('.about-section').waypoint(
+            function(direction){
+                if(direction == "down"){
+                    $('nav').addClass('sticky');
+                }
+                else{
+                    $('nav').removeClass('sticky');
+                }
+            }, {
+                offset: '400px'
+            }
+        )
+        // Scroll
+        $('a').click(function(event){
+            $('html, body').animate({
+                scrollTop: $($.attr(this,'href')).offset().top
+            },1000);
+            event.preventDefault();
+        });
+
+        //mobile navigation
+        $('.mobile-nav-icon').click(
+            function(){
+                $('.main-nav').slideToggle(200);
+                if($('.mobile-nav-icon').hasClass('fa-bars')){
+                    $('.mobile-nav-icon').addClass('fa-times');
+                    $('.mobile-nav-icon').removeClass('fa-bars');
+                }
+                else{
+                    $('.mobile-nav-icon').addClass('fa-bars');
+                    $('.mobile-nav-icon').removeClass('fa-times');
+                }
+            }
+        )
+    }
+)
